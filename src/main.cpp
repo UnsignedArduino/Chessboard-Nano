99625a75a371e69c55243a9821f609c894676611
Cyrus Yiu <UnsignedArduino@outlook.com>
2025-03-06 23:03:52 -0500

    Print out all 8 columns and rows

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,20 @@
 #include <Arduino.h>
 
-const uint8_t EXPANDERS_NUM = 8;
+const uint8_t CHESSBOARD_ROWS = 8;
+const uint8_t CHESSBOARD_COLS = 8;
+uint16_t linearHallValues[CHESSBOARD_ROWS][CHESSBOARD_COLS];
+
+const uint8_t EXPANDERS_NUM = CHESSBOARD_ROWS;
 const uint8_t EXPANDERS_A_PIN = 2;
 const uint8_t EXPANDERS_B_PIN = 3;
 const uint8_t EXPANDERS_C_PIN = 4;
 const uint8_t EXPANDERS_INH_PIN = 5;
 const uint8_t EXPANDER_COMS_PINS[EXPANDERS_NUM] = {A7, A6, A5, A4,
                                                    A3, A2, A1, A0};
-// Need to read expander values in this order (due to wiring)
-const uint8_t EXPANDER_COL_TO_BIS[EXPANDERS_NUM] = {2, 1, 0, 3, 5, 7, 6, 4};
+// Need to read expander in this order (due to wiring)
+const uint8_t EXPANDER_COLS_TO_BITS[CHESSBOARD_COLS] = {2, 1, 0, 3, 5, 7, 6, 4};
 
-const uint8_t CHESSBOARD_ROWS = 8;
-const uint8_t CHESSBOARD_COLS = 8;
-uint16_t linHallVals[CHESSBOARD_ROWS][CHESSBOARD_COLS];
-
-void setup() {
-  Serial.begin(9600);
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, LOW);
-
+void linearHallsBegin() {
   pinMode(EXPANDERS_A_PIN, OUTPUT);
   pinMode(EXPANDERS_B_PIN, OUTPUT);
   pinMode(EXPANDERS_C_PIN, OUTPUT);
@@ -30,21 +26,39 @@ void setup() {
   for (uint8_t i : EXPANDER_COMS_PINS) {
     pinMode(i, INPUT);
   }
-  memset(linHallVals, 0, sizeof(linHallVals));
+  memset(linearHallValues, 0, sizeof(linearHallValues));
+}
+
+void linearHallsRead() {
+  for (uint8_t col = 0; col < CHESSBOARD_COLS; col++) {
+    digitalWrite(EXPANDERS_A_PIN, EXPANDER_COLS_TO_BITS[col] & 0b001);
+    digitalWrite(EXPANDERS_B_PIN, EXPANDER_COLS_TO_BITS[col] & 0b010);
+    digitalWrite(EXPANDERS_C_PIN, EXPANDER_COLS_TO_BITS[col] & 0b100);
+    delay(10);
+    for (uint8_t row = 0; row < CHESSBOARD_ROWS; row++) {
+      linearHallValues[row][col] = analogRead(EXPANDER_COMS_PINS[row]);
+    }
+  }
+}
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+
+  linearHallsBegin();
 }
 
 void loop() {
-  // For now only read first row
-  for (uint8_t i : EXPANDER_COL_TO_BIS) {
-    digitalWrite(EXPANDERS_A_PIN, i & 0b001);
-    digitalWrite(EXPANDERS_B_PIN, i & 0b010);
-    digitalWrite(EXPANDERS_C_PIN, i & 0b100);
-    delay(10);
-    Serial.print(map(analogRead(EXPANDER_COMS_PINS[0]), 0, 1023, 0, 5000));
-    Serial.print(" ");
+  linearHallsRead();
+
+  for (uint8_t row = 0; row < CHESSBOARD_ROWS; row++) {
+    for (uint8_t col = 0; col < CHESSBOARD_COLS; col++) {
+      Serial.print(linearHallValues[row][col]);
+      Serial.print(" ");
+    }
+    Serial.println();
   }
   Serial.println();
-  Serial.println();
-
-  delay(1000);
+  delay(200);
 }
